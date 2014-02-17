fc4e4cabe6a0223b976b9d4358a1015cef46c219
Damien Mathieu <42@dmathieu.com>
2014-02-17 16:17:22 +0100

    import the library, not the main package

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/dmathieu/deploy_docus"
 	"fmt"
+	"github.com/dmathieu/deploy_docus/deploy_docus"
 	"os"
 	"strconv"
 )
@@ -23,17 +23,17 @@ func getPort() int64 {
 }
 
 func deploy(message deploy_docus.Message) {
-  fmt.Println("I just received a message:", message)
+	fmt.Println("I just received a message:", message)
 
-  err := deploy_docus.NewCloner(message.Repository).Fetch()
-  if err != nil {
-    panic(err)
-  }
+	err := deploy_docus.NewCloner(message.Repository).Fetch()
+	if err != nil {
+		panic(err)
+	}
 
-  err = deploy_docus.NewPusher(&message).Push()
-  if err != nil {
-    panic(err)
-  }
+	err = deploy_docus.NewPusher(&message).Push()
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
@@ -46,6 +46,6 @@ func main() {
 
 	for {
 		message := <-channel
-    go deploy(message)
+		go deploy(message)
 	}
 }
