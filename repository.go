08e7ba7616486c99e72a60a60bba1cc2b25e0d25
Damien Mathieu <42@dmathieu.com>
2014-02-17 14:18:54 +0100

    write the repository's private key to disk so we can use it later

--- repository.go
+++ repository.go
@@ -2,13 +2,16 @@ package deploy_docus
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
 type Repository struct {
 	Origin      string
 	Destination string
+	PKey        string
 }
 
 func (r *Repository) Name() string {
@@ -23,13 +26,31 @@ func (r *Repository) LocalPath() string {
 	return fmt.Sprintf("/tmp/%s", r.Name())
 }
 
+func (r *Repository) CreatePKey() {
+	path := r.PKeyPath()
+
+	err := os.MkdirAll(filepath.Dir(path), 0700)
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile(path, []byte(r.PKey), 0700)
+	if err != nil {
+		panic(err)
+	}
+}
+
 func (r *Repository) PKeyPath() string {
 	return fmt.Sprintf("/tmp/deploy_docus/keys/%s", r.Name())
 }
 
 func FindRepository() *Repository {
-	return &Repository{
+	repository := &Repository{
 		Origin:      os.Getenv("REPOSITORY_ORIGIN"),
 		Destination: os.Getenv("REPOSITORY_DESTINATION"),
+		PKey:        os.Getenv("REPOSITORY_PKEY"),
 	}
+	repository.CreatePKey()
+
+	return repository
 }
